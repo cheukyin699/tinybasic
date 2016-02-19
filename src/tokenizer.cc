93979fed283ab46bbf2e25af455b41b731e3f674
Cheuk Yin Ng <cheukyin699@yahoo.com>
2016-02-19 15:05:13 -0800

    Some work on the tokenizer

--- src/tokenizer.cc
+++ src/tokenizer.cc
@@ -1,1 +1,23 @@
 #include "tokenizer.h"
+
+vector<token_t> get_tokens(string line) {
+    vector<token_t> tokens;
+    token_t tmp;
+
+    for (unsigned i = 0; i < line.size(); i++) {
+        if (line[i] == '"' && tmp.t == NONE) {
+            // Check for a string and put it into string mode
+            tmp.t = STRING;
+        }
+        else if (line[i] == '"' && tmp.t == STRING) {
+            // If you are done with the string, append and reset it
+            tokens.append(tmp);
+        }
+        else {
+            tmp.str += line[i];
+        }
+    }
+
+    return tokens;
+}
+
--- src/tokenizer.h
+++ src/tokenizer.h
@@ -1,17 +1,36 @@
 #ifndef TOKENIZER_H
 #define TOKENIZER_H
-/*
- * All these things that are binary operators would be converted from char to
- * int. This includes the following:
- *
- * +*-/<>=
+
+#include <string>
+#include <vector>
+
+/* A representation of all the tokens
  */
 enum Token {
+    // Nothing
+    NONE,
+    // Constants
     NUMBER, STRING,
-    // Doubled binary operators
-    LTE, GTE,
+    // Binary operators
+    ADD, SUB, MUL, DIV, EQ, NEQ, LT, GT, LTE, GTE,
     // Reserved Keywords
     PRINT, IF, THEN, GOTO, INPUT, LET, GOSUB, RETURN, CLEAR, LIST, RUN, END
 };
+
+/* A representation of the token type, in which contains the type of the token,
+ * as well as the value(s)
+ */
+struct token_t {
+    Token t;
+    int num;
+    string str;
+};
+
+// TODO
+token_t::token_t(Token tt=NONE, int n=0, string s="");
+
+/* Get's a list of tokens from the line and returns them as a vector
+ */
+vector<token_t> get_tokens(string line);
 
 #endif
